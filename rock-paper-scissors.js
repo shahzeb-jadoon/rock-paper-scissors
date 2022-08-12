a4390aeeaf2724a6febc338288cd69f857beb09b
Shahzeb Jadoon <shahzeb.jadoon.sj@gmail.com>
2022-08-12 00:05:47 -0500

    Update user choice funtion

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,7 +8,7 @@ function getComputerChoice() {
 
 // Get input from user
 function getUserChoice() {
-    userChoice = (toString(prompt("Rock(0), Paper(1), or Scissors(2)?"))).slice(0, 1).toLowerCase();
+    userChoice = (prompt("Rock(0), Paper(1), or Scissors(2)?")).slice(0, 1).toLowerCase();
     if (userChoice === 0 | userChoice === "r") {
         return 0;
     } else if (userChoice === 1 | userChoice === "p") {
@@ -16,7 +16,11 @@ function getUserChoice() {
     } else if (userChoice === 2 | userChoice === "s") {
         return 2;
     } else if (userChoice === "") {
-        return "No response"
+        alert("Please enter a choice!");
+        return "No Response";
+    } else {
+        alert("Invalid Response!");
+        return "Invalid Response"
     }
 }
 
