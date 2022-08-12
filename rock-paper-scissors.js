13beff7910b21e221eda386fae3418dd32396880
Shahzeb Jadoon <shahzeb.jadoon.sj@gmail.com>
2022-08-11 23:26:49 -0500

    Define computer's turn

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,2 +1,7 @@
 // This game allows the user to play Rock Paper Scissors against the
 // computer.
+
+// We start by retrieving a random choice for the computer
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3)
+}
